c9ce958a12e9943aff1efeb9d4e1f2e10698c46e
jkjoy99 <jkjoy99@gmail.com>
2023-05-15 15:04:34 +0600

    first commit

---
+++ src/components/AddCoffee.jsx
@@ -0,0 +1,120 @@
+
+import React from 'react';
+import Swal from 'sweetalert2'
+
+const AddCoffee = () => {
+    const handleAddCoffee = event =>{
+        event.preventDefault();
+        const form =event.target
+        const name = form.name.value;
+        const quantity = form.quantity.value;
+        const supplier = form.supplier.value;
+        const taste = form.taste.value;
+        const category = form.category.value;
+        const details = form.details.value;
+        const photo = form.photo.value;
+        const newCoffee = {name,quantity,supplier,taste,category,details,photo}
+        console.log(newCoffee);
+
+        // send data to the server
+        fetch('http://localhost:5000/coffee',{
+            method:'POST',
+            headers:{
+                'content-type': 'application/json'
+            },
+            body:JSON.stringify(newCoffee)
+        })
+        .then(res => res.json())
+        .then(data => {
+            console.log(data);
+            if(data.insertedId){
+                Swal.fire({
+                    title: 'Success',
+                    text: 'Coffee Added Successfully',
+                    icon: 'success',
+                    confirmButtonText: 'ok'
+                  })
+                  form.reset()
+            }
+        })
+
+    }
+    return (
+        <div className='bg-[#F4F3F0] p-24'>
+            <h1 className='text-center text-6xl text-purple-600'>AddCoffee111</h1>
+            <form onSubmit={handleAddCoffee}>
+                {/* form name name and quantity */}
+                <div className='md:flex mb-8 gap-4'>
+                    <div className="form-control md:w-1/2">
+                        <label className="label">
+                            <span className="label-text">Coffee Name</span>
+                        </label>
+                        <label className="input-group">
+                            <input type="text" name='name' placeholder="Coffee Name" className="input input-bordered w-full" />
+                        </label>
+                    </div>
+                    <div className="form-control md:w-1/2">
+                        <label className="label">
+                            <span className="label-text">Available Quantity</span>
+                        </label>
+                        <label className="input-group">
+                            <input type="text" name='quantity' placeholder="Available Quantity" className="input input-bordered w-full" />
+                        </label>
+                    </div>
+                </div>
+                {/* form supplier row */}
+                <div className='md:flex mb-8 gap-4'>
+                    <div className="form-control md:w-1/2">
+                        <label className="label">
+                            <span className="label-text">Supplier Name</span>
+                        </label>
+                        <label className="input-group">
+                            <input type="text" name='supplier' placeholder="Supplier Name" className="input input-bordered w-full" />
+                        </label>
+                    </div>
+                    <div className="form-control md:w-1/2">
+                        <label className="label">
+                            <span className="label-text">Taste</span>
+                        </label>
+                        <label className="input-group">
+                            <input type="text" name='taste' placeholder="Taste" className="input input-bordered w-full" />
+                        </label>
+                    </div>
+                </div>
+                {/* form category and details */}
+                <div className='md:flex mb-8 gap-4'>
+                    <div className="form-control md:w-1/2">
+                        <label className="label">
+                            <span className="label-text">Category</span>
+                        </label>
+                        <label className="input-group">
+                            <input type="text" name='category' placeholder="Category Name" className="input input-bordered w-full" />
+                        </label>
+                    </div>
+                    <div className="form-control md:w-1/2">
+                        <label className="label">
+                            <span className="label-text">Details</span>
+                        </label>
+                        <label className="input-group">
+                            <input type="text" name='details' placeholder="Details" className="input input-bordered w-full" />
+                        </label>
+                    </div>
+                </div>
+                {/* form photo url row */}
+                <div className='gap-4 mb-8'>
+                    <div className="form-control w-full">
+                        <label className="label">
+                            <span className="label-text">Photo url</span>
+                        </label>
+                        <label className="input-group">
+                            <input type="text" name='photo' placeholder="Photo url" className="input input-bordered w-full" />
+                        </label>
+                    </div>
+                </div>
+                <input type="submit" value="Add Coffee" className="btn btn-block" />
+            </form>
+        </div>
+    );
+};
+
+export default AddCoffee;
